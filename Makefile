b0fea3dd02515fad5d6130b274c4f731b51e2e75
Joone Hur <joone.hur@intel.com>
2017-10-18 18:40:19 -0700

    Add a triagle animation example

---
+++ Makefile
@@ -0,0 +1,12 @@
+LIBS = -lGLESv2 -lEGL -lm -lX11  -lcairo -lwayland-client -lwayland-server -lwayland-cursor -lwayland-egl
+CFLAGS = -I/usr/include/cairo -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/freetype2 -I/usr/include/libdrm -I/usr/include/libpng12  -I/usr/include
+
+all: triangle_animation \
+
+triangle_animation : ./2.triangle_animation/main.o  ./common/common.o
+	gcc ./2.triangle_animation/main.c ./common/common.c ${CFLAGS} -o $@ ${LIBS}
+
+
+
+clean:
+	rm -f *.o *~ 
---
+++ common/common.h
@@ -0,0 +1,55 @@
+#include <wayland-client.h>
+#include <wayland-egl.h>
+#include <wayland-cursor.h>
+
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+
+
+struct window;
+struct seat;
+
+struct display {
+	struct wl_display *display;
+	struct wl_registry *registry;
+	struct wl_compositor *compositor;
+	struct wl_shell *shell;
+	struct wl_seat *seat;
+	struct wl_pointer *pointer;
+	struct wl_keyboard *keyboard;
+	struct wl_shm *shm;
+	struct wl_cursor_theme *cursor_theme;
+	struct wl_cursor *default_cursor;
+	struct wl_surface *cursor_surface;
+	struct {
+		EGLDisplay dpy;
+		EGLContext ctx;
+		EGLConfig conf;
+	} egl;
+	struct window *window;
+};
+
+struct geometry {
+	int width, height;
+};
+
+struct window {
+	struct display *display;
+	struct geometry geometry, window_size;
+	struct {
+		GLuint rotation_uniform;
+		GLuint pos;
+		GLuint col;
+	} gl;
+
+	struct wl_egl_window *native;
+	struct wl_surface *surface;
+	struct wl_shell_surface *shell_surface;
+	EGLSurface egl_surface;
+	struct wl_callback *callback;
+	int fullscreen, configured, opaque;
+};
+
+void init_egl(struct display *display, int opaque);
+void fini_egl(struct display *display);
+void init_gl(struct window *window, const char* vert_shader_text, const char* frag_shader_text);
